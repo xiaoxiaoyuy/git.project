e35a92f01f7de7d2a61285bd28f1f64e416e3666
xiaoxiaoyuy <1076202404@qq.com>
2019-05-16 09:31:27 +0800

    no.1

---
+++ student/src/com/student/model/StudentModel.java
@@ -0,0 +1,40 @@
+package com.student.model;
+
+public class StudentModel {
+	private int id;
+	private String name;
+	private int sex;
+	private int age;
+	private String love;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSex() {
+		return sex;
+	}
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getLove() {
+		return love;
+	}
+	public void setLove(String love) {
+		this.love = love;
+	}
+	
+}
